5069e271060ae0a34c1e5112236c8ebb3b3173a4
Adam Booth <adam@ambooth.com>
2014-02-17 21:13:51 -0600

    attempt to fix heroku installation

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'app')));
-// var messages = [];
-// var sockets = [];
 
 io.on('connection', function (socket) {
     // console.log(socket.id);
@@ -31,12 +29,12 @@ io.on('connection', function (socket) {
     });
 
     socket.on('tankupdate', function(data) {
-        console.log('tu', data);
+//        console.log('tu', data);
         socket.broadcast.emit('tankmove',data);
     });
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
     var addr = server.address();
-    console.log("Chat server listening at", addr.address + ":" + addr.port);
+    console.log("Game server listening at", addr.address + ":" + addr.port);
 });
