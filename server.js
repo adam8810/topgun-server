b7c878485f9d455285f28bb8d8b3ee8459dad863
Adam Booth <adam@ambooth.com>
2014-02-18 20:20:46 -0600

    Rearrange express calls

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var io = socketio.listen(server);
 
 
 router.use(express.static(path.resolve(__dirname, 'app')));
-router.use('/bower_components', express.static(__dirname + '/bower_components'));
+router.use('/bower', express.static(__dirname + '/../bower_components'))
 
 io.on('connection', function (socket) {
     // console.log(socket.id);
